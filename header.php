d426f682214a64b5a2a4a987058014df22cc5e63
calvin224 <calvinpower44@gmail.com>
2022-03-06 00:22:55 +0000

    Sign-in Bug

--- header.php
+++ header.php
@@ -5,5 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Match-It!</title>
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
