2a0366e8c91ae5c835b40b0a1b575b040b0da5a3
jackboland <63565902+jackboland@users.noreply.github.com>
2022-03-21 18:07:28 +0000

    Add files via upload

---
+++ Profile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/Template.css">
+        <script src="https://kit.fontawesome.com/b17df002ae.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+    <div class="wrapper">
+        <header>
+            <div class="header metrics">
+                <button> <a href="Welcome.php"> <i class="fa-solid fa-rocket"></i> </a> </button>
+            </div>
+            <div class="header chat">
+                <button> <a href="Chat.php"> <i class="fa-solid fa-paper-plane"></i> </a> </button>
+            </div>
+            <div class="header profilepicture">
+                <button> <a href="Profile.php"> <img src="php/images/1646961640Untitled123132.png"> </a> </button>
+            </div>
+        </header>
+        <hr>
+            <div class="profile columns">
+                <div class="profile col1">
+                    <div class="profile picture">
+                        <img src="php/images/1646961640Untitled123132.png">
+                    </div>
+                    <div class="profile attributes">
+                        <img src="php/images/freshhatch.png" title="New Hatch">
+                        <img src="php/images/frosty.png" title="Frosty!">
+                        <img src="php/images/inshell.png" title="In Your Shell">
+                        <img src="php/images/onfire.png" title="On Fire!">
+                    </div>
+                    <div class="profile details">
+                        <p>Jack</p>
+                        <p>20</p>
+                        <p>Limerick</p>
+                    </div>
+                    <div class="profile about">
+                        <p>Fusce mattis pulvinar tortor a vehicula. Mauris turpis tellus, porttitor sit amet egestas id, congue et dui. Phasellus feugiat, risus quis tincidunt auctor, augue est mattis erat, id volutpat lectus nunc in leo. Aliquam non efficitur mi. Etiam sagittis, turpis at mollis bibendum, eros tellus consectetur felis, eu convallis felis lacus sed nisi. Cras aliquet feugiat leo vel mollis. Quisque vitae libero vehicula, rhoncus erat vel, auctor purus. Nam venenatis laoreet pharetra. Quisque mauris mauris, condimentum ac nunc nec, ultrices semper massaFusce mattis pulvinar tortor a vehicula. Mauris turpis tellus, porttitor sit amet egestas id, congue et dui. Phasellus feugiat, risus quis tincidunt auctor, augue est mattis erat, id volutpat lectus nunc in leo. Aliquam non efficitur mi. Etiam sagittis, turpis at mollis bibendum, eros tellus consectetur felis, eu convallis felis lacus sed nisi. Cras aliquet feugiat leo vel mollis. Quisque vitae libero vehicula, rhoncus erat vel, auctor purus. Nam venenatis laoreet pharetra. Quisque mauris mauris, condimentum ac nunc nec, ultrices semper massa.</p>
+                    </div>
+                </div>
+                <div class="profile col2">
+                    <div class="bioheader">
+                        <p>User Bio</p>
+                    </div>
+                    <div class="bio">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt vehicula dictum. In hac habitasse platea dictumst. Vestibulum aliquet imperdiet velit, et euismod nunc blandit sed. Curabitur hendrerit elit in tincidunt semper. Nulla semper ex tellus, sit amet dictum nisi ultrices ac. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin consectetur, lacus in suscipit pharetra, nisi leo ultricies diam, quis suscipit dolor sem vitae ipsum. Pellentesque faucibus euismod condimentum.</p>
+                    </div>
+                </div>
+                <div class="profile col3">
+                    <div class="gallery">
+                        <img src="php/images/1646743105discordpic.png">
+                        <img src="php/images/1646743105discordpic.png">
+                        <img src="php/images/1646743105discordpic.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
