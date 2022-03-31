fef0d2943bec2608fbec9f9f77af97c7feb7a7bf
calvin224 <53382194+calvin224@users.noreply.github.com>
2022-03-31 21:09:23 +0100

    Update Edit.php

--- php/Edit.php
+++ php/Edit.php
@@ -10,10 +10,13 @@ $Gender = mysqli_real_escape_string($conn, $_POST['Gender']);
 $Seeking = mysqli_real_escape_string($conn, $_POST['Seeking']);
 $Location = mysqli_real_escape_string($conn, $_POST['Location']);
 $Description = mysqli_real_escape_string($conn, $_POST['Description']);
+$completed = 1;
         $sql = mysqli_query($conn, "SELECT * FROM profiletable WHERE unique_id =  {$_SESSION['unique_id']}");
         if(mysqli_num_rows($sql) > 0){
             $insert_query = mysqli_query($conn, "UPDATE profiletable 
             SET Age = '{$Age}',Gender ='{$Gender}' ,Seeking='{$Seeking}',Description='{$Description}',Location='{$Location}' WHERE unique_id = '{$id}'");
+            $insert_query = mysqli_query($conn, "UPDATE users 
+            SET Completed = '{$completed}' WHERE unique_id = '{$id}'");
             echo "success";
         }else {
             $time = time();
