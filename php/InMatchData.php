246ffba6155125f1c0651a939511674abee73893
calvin224 <53382194+calvin224@users.noreply.github.com>
2022-03-31 21:28:12 +0100

    Update InMatchData.php

--- php/InMatchData.php
+++ php/InMatchData.php
@@ -6,7 +6,7 @@ while($row = mysqli_fetch_assoc($query)){
                 OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
     $query2 = mysqli_query($conn, $sql2);
     $row2 = mysqli_fetch_assoc($query2);
-    (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
+    (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="";
     (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
     if(isset($row2['outgoing_msg_id'])){
         ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
