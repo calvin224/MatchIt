d492190e48c9938dd6507aafdd1b1663edd04704
calvin224 <53382194+calvin224@users.noreply.github.com>
2022-03-31 21:25:05 +0100

    Add files via upload

--- users.php
+++ users.php
@@ -4,6 +4,7 @@ include_once "php/config.php";
 if(!isset($_SESSION['unique_id'])){
     header("location: LoginPage.php");
 }
+
 $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
 if (mysqli_num_rows($sql) > 0) {
     $row = mysqli_fetch_assoc($sql);
