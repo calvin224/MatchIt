9c6fe9c95994335ceec3d3b26a4cb8e98bb9b3be
Cian Shanahan <55852192+cian-shan@users.noreply.github.com>
2022-04-25 22:06:16 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,11 +1,11 @@
 <?php
 $hostname = "localhost";
-$username = "root";
-$password = "";
-$dbname = "matchit";
+$username = "id18657714_matchitadmin";
+$password = "@zTqe7/L34V4gJx=";
+$dbname = "id18657714_matchit";
 
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if(!$conn){
-  echo "Database connection error".mysqli_connect_error();
+    echo "Database connection error".mysqli_connect_error();
 }
 ?>
