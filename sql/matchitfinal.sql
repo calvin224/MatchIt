2d4977f0116597c3fe43ee713bf656d64dfc2992
jackboland <63565902+jackboland@users.noreply.github.com>
2022-04-27 15:01:10 +0100

    Add files via upload

--- sql/matchitfinal.sql
+++ sql/matchitfinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2022 at 03:57 PM
+-- Generation Time: Apr 27, 2022 at 04:00 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -45,6 +45,43 @@ CREATE TABLE `availableabouttable` (
   `Icon` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `availableabouttable`
+--
+
+INSERT INTO `availableabouttable` (`AboutID`, `Name`, `Icon`) VALUES
+(2, 'Blonde Hair', '1'),
+(3, 'Black Hair', '1'),
+(4, 'Red Hair', '1'),
+(5, 'Dyed Hair', '1'),
+(6, 'Short Hair', '1'),
+(7, 'Medium Length Hair', '1'),
+(8, 'Long Hair', '1'),
+(9, 'Curly Hair', '1'),
+(10, 'Straight Hair', '1'),
+(11, 'Wavy Hair', '1'),
+(12, 'Blue Eyes', '1'),
+(13, 'Brown Eyes', '1'),
+(14, 'Black Eyes', '1'),
+(15, 'Hazel Eyes', '1'),
+(16, 'Grey Eyes', '1'),
+(17, 'Green Eyes', '1'),
+(18, 'Amber Eyes', '1'),
+(19, 'Violet Eyes', '1'),
+(20, 'Short', '1'),
+(21, 'Average', '1'),
+(22, 'Tall', '1'),
+(23, 'Skinny', '1'),
+(24, 'Average', '1'),
+(25, 'Full Figured', '1'),
+(26, 'Dad Bod', '1'),
+(27, 'Freckles', '1'),
+(28, 'Facial Hair', '1'),
+(29, 'Piercings', '1'),
+(30, 'Tattoos', '1'),
+(31, 'Introvert', '1'),
+(32, 'Extrovert', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +93,81 @@ CREATE TABLE `availablehobbiestable` (
   `Name` varchar(255) DEFAULT NULL,
   `Icon` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `availablehobbiestable`
+--
+
+INSERT INTO `availablehobbiestable` (`InterestID`, `Name`, `Icon`) VALUES
+(1, 'Acting', NULL),
+(2, 'American Football', NULL),
+(3, 'Animals', NULL),
+(4, 'Archery', NULL),
+(5, 'Art', NULL),
+(6, 'Badminton', NULL),
+(7, 'Baseball', NULL),
+(8, 'Basketball', NULL),
+(9, 'Board Games', NULL),
+(10, 'Boxing', NULL),
+(11, 'Camogie', NULL),
+(12, 'Camping', NULL),
+(13, 'Carnivore', NULL),
+(14, 'Cars', NULL),
+(15, 'Cat Person', NULL),
+(16, 'Coffee', NULL),
+(17, 'Comedy', NULL),
+(18, 'Cooking', NULL),
+(19, 'Cricket', NULL),
+(20, 'Crypto', NULL),
+(21, 'Cycling', NULL),
+(22, 'Dancing', NULL),
+(23, 'Disney', NULL),
+(24, 'Dog Person', NULL),
+(25, 'Farming', NULL),
+(26, 'Fashion', NULL),
+(27, 'Fishing', NULL),
+(28, 'Food', NULL),
+(29, 'Formula 1', NULL),
+(30, 'Gaelic Football', NULL),
+(31, 'Gaming', NULL),
+(32, 'Gardening', NULL),
+(33, 'Golf', NULL),
+(34, 'Gym', NULL),
+(35, 'Handball', NULL),
+(36, 'Hiking', NULL),
+(37, 'Hockey', NULL),
+(38, 'Hurling', NULL),
+(39, 'MMA', NULL),
+(40, 'Marvel', NULL),
+(41, 'Movies', NULL),
+(42, 'Music', NULL),
+(43, 'NFTs', NULL),
+(44, 'Netflix', NULL),
+(45, 'Omnivore', NULL),
+(46, 'Outdoors', NULL),
+(47, 'Pescitarian', NULL),
+(48, 'Photography', NULL),
+(49, 'Reading', NULL),
+(50, 'Rugby', NULL),
+(51, 'Running', NULL),
+(52, 'Sailing', NULL),
+(53, 'Shopping', NULL),
+(54, 'Singing', NULL),
+(55, 'Soccer', NULL),
+(56, 'Social Networking', NULL),
+(57, 'Star Wars', NULL),
+(58, 'Surfing', NULL),
+(59, 'Swimming', NULL),
+(60, 'Tea', NULL),
+(61, 'Technology', NULL),
+(62, 'Tennis', NULL),
+(63, 'Vegan', NULL),
+(64, 'Vegetarian', NULL),
+(65, 'Volunteering', NULL),
+(66, 'Walking', NULL),
+(67, 'Writing', NULL),
+(68, 'Yoga', NULL),
+(69, 'Zumba', NULL);
 
 -- --------------------------------------------------------
 
